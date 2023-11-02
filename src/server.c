59df32a6957a23de7851991763de6a846e4e4ccf
Iliya Iliev <iliyailiev3592@gmail.com>
2023-11-02 16:46:48 +0200

    app: server: Add UDP server
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- src/server.c
+++ src/server.c
@@ -10,23 +10,148 @@
 
 /******************************** LOCAL DEFINES *******************************/
 LOG_MODULE_REGISTER(server, LOG_LEVEL_DBG);
+#define SERVER_IO_BUFFER_SIZE 1000
 
 /********************************* TYPEDEFS ***********************************/
+typedef int (*server_bind_t)(server_t *);
+typedef int (*server_process_t)(server_t *);
+
+typedef struct _server_iface_t {
+    server_bind_t bind;
+    server_process_t process;
+} server_iface_t;
+typedef server_iface_t *(*server_iface_create_t)(void);
+
+
 struct _server_t
 {
+    server_proto_t type;
+    int port;
     int sock;
+    server_iface_t *iface;
 };
+
 /********************************* LOCAL DATA *********************************/
 
+
 /******************************* LOCAL FUNCTIONS ******************************/
+static int server_udp_bind(server_t *self)
+{
+    int ret = 0;
+    struct sockaddr_in s_addr;
+    socklen_t s_addr_len = 0;
+
+    if (self->type != SERVER_PROTO_UDP)
+    {
+        LOG_ERR("Invalid server context!");
+        return -1;
+    }
+
+    (void)memset(&s_addr, 0, sizeof(s_addr));
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_port = htons(self->port);
+
+    self->sock = socket(s_addr.sin_family, SOCK_DGRAM, IPPROTO_UDP);
+    if (self->sock < 0) {
+        LOG_ERR("Failed to create UDP socket: %d, %s", errno, strerror(errno));
+        ret = -errno;
+    }
+
+    if (0 == ret) {
+        s_addr_len = sizeof(s_addr);
+        ret = bind(self->sock, (struct sockaddr *)&s_addr, s_addr_len);
+        if (ret < 0) {
+            LOG_ERR("Failed to bind UDP socket: %d, %s", errno, strerror(errno));
+            ret = -errno;
+        }
+    }
+
+    return ret;
+}
+
+static int server_udp_process(server_t *self)
+{
+    int ret = 0;
+    int received;
+    struct sockaddr client_addr;
+    socklen_t client_addr_len;
+    char recv_buffer[SERVER_IO_BUFFER_SIZE];
+
+    if (self->type != SERVER_PROTO_UDP)
+    {
+        LOG_ERR("Invalid server context!");
+        return -1;
+    }
+
+    LOG_INF("Listeing on UDP port %d...", self->port);
+    for(;;)
+    {
+        client_addr_len = sizeof(client_addr);
+        received = recvfrom(self->sock, recv_buffer,
+                    SERVER_IO_BUFFER_SIZE, 0, &client_addr, &client_addr_len);
+        if (received < 0) {
+            /* Socket error */
+            NET_ERR("UDP: Connection error: %d, %s", errno, strerror(errno));
+            continue;
+        }
+        LOG_DBG("Received: %d bytes", received);
+
+        ret = sendto(self->sock, recv_buffer, received, 0,
+                 &client_addr, client_addr_len);
+        if (ret < 0) {
+            NET_ERR("UDP: Failed to send: %d, %s", errno, strerror(errno));
+            continue;
+        }
+    }
+}
+
+static server_iface_t *server_udp_create(void)
+{
+    server_iface_t *udp = (server_iface_t *) k_malloc (sizeof (server_iface_t));
+    udp->bind = server_udp_bind;
+    udp->process = server_udp_process;
+
+    return udp;
+}
+
+static int server_tcp_bind(server_t *self)
+{
+    LOG_WRN("Not implemented");
+
+    return -1;
+}
+
+static int server_tcp_process(server_t *self)
+{
+    LOG_WRN("Not implemented");
+
+    return -1;
+}
+
+static server_iface_t *server_tcp_create(void)
+{
+    server_iface_t *tcp = (server_iface_t *) k_malloc (sizeof (server_iface_t));
+    tcp->bind = server_tcp_bind;
+    tcp->process = server_tcp_process;
+
+    return tcp;
+}
+
+static server_iface_create_t server_iface_create[SERVER_PROTO_N] = {
+    server_udp_create,
+    server_tcp_create,
+};
 
 /***************************** INTERFACE FUNCTIONS ****************************/
-server_t *server_new (void)
+server_t *server_new (server_proto_t type, int port)
 {
     LOG_INF("Creating new server object");
 
     server_t *self = (server_t *) k_malloc (sizeof (server_t));
     assert (self);
+    self->type = type;
+    self->port = port;
+    self->iface = server_iface_create[type]();
 
     return self;
 }
@@ -38,7 +163,23 @@ void server_destroy (server_t **self_p)
         server_t *self = *self_p;
         //  Free class properties here
         //  Free object itself
+        k_free(self->iface);
         k_free (self);
         *self_p = NULL;
     }
+}
+
+void server_start (void *p1, void *p2, void *p3)
+{
+    int ret = 0;
+    server_t *self = (server_t *)p1;
+
+    ret = self->iface->bind(self);
+    if (0 != ret)
+    {
+        LOG_ERR("Bind Failed!");
+        return;
+    }
+
+    self->iface->process(self);
 }
--- src/server.h
+++ src/server.h
@@ -4,6 +4,15 @@
 
 /********************************* TYPEDEFS ***********************************/
 typedef struct _server_t server_t;
+
+typedef enum _server_proto_t
+{
+    SERVER_PROTO_UDP = 0,
+    SERVER_PROTO_TCP,
+    SERVER_PROTO_N
+} server_proto_t;
+
 /***************************** INTERFACE FUNCTIONS ****************************/
-server_t *server_new (void);
+server_t *server_new (server_proto_t type, int port);
 void server_destroy (server_t **self_p);
+void server_start (void *p1, void *p2, void *p3);
