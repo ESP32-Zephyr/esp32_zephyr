217bcfa6b6227e24a4d2dea74f4e190f0db72e7b
Iliya Iliev <iliyailiev3592@gmail.com>
2023-11-02 12:00:48 +0200

    app: Add server constructor/destructor to main
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,14 +1,26 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <stddef.h>
+
 #include "wifi.h"
+#include "server.h"
 
 int main(void)
 {
-    wifi_iface_t *wifi = wifi_get();
+    wifi_iface_t *wifi = NULL;
+    server_t *server = NULL;
 
+    wifi = wifi_get();
     wifi->connect(SSID, PSK);
     wifi->status();
+
+    server = server_new();
+
+
+
+    server_destroy(&server);
+
     wifi->disconnect();
 
     return 0;
