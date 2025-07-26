c98e79f33581b96a1773b1aee794d7f4eda343da
Iliya Iliev <iliyailiev3592@gmail.com>
2025-07-26 23:14:45 +0300

    app: cmd_handler: Add response formatter helper function
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- src/cmd_interpreter.c
+++ src/cmd_interpreter.c
@@ -16,9 +16,26 @@ LOG_MODULE_REGISTER(cmd_interpreter, LOG_LEVEL_DBG);
 static cmd_hndlr_t cmd_handlers[_command_id_ARRAYSIZE];
 
 /******************************** LOCAL FUNCTIONS *****************************/
+static void response_set_hdr(response *res, command_id cmd_id, ret_code ret, const char *err_msg)
+{
+    res->hdr.id = cmd_id;
+    res->hdr.has_ret = true;
+    if (ret != ret_code_OK && err_msg != NULL) {
+        res->hdr.ret = ret;
+        strcpy(res->hdr.err_msg, err_msg);
+        res->hdr.has_err_msg = true;
+    }
+    else
+    {
+        res->hdr.ret = ret;
+        res->hdr.has_err_msg = false;
+    }
+
+}
+
 static int request_version_get_handler(const request *req, response *res)
 {
-    LOG_INF("%s\n", __func__);
+    LOG_DBG("%s", __func__);
     (void)req;
 
     res->pl.version_get.version = APP_VERSION;
@@ -26,23 +43,18 @@ static int request_version_get_handler(const request *req, response *res)
     strcpy(res->pl.version_get.sha1, git_CommitSHA1());
     strcpy(res->pl.version_get.commit_date, git_CommitDate());
     res->which_pl = response_version_get_tag;
-    res->hdr.ret = ret_code_OK;
-    res->has_hdr = 1;
-
-    //res->hdr.ret = ret_code_ERROR;
-    //strcpy(res->hdr.err_msg, "Failed!");;
+    response_set_hdr(res, command_id_VERSION_GET, ret_code_OK, NULL);
 
     return 0;
 }
 
 static int request_adc_chs_get_handler(const request *req, response *res)
 {
-    LOG_INF("%s\n", __func__);
+    LOG_DBG("%s", __func__);
     (void)req;
-
     res->pl.adc_chs_get.adc_chs = 2;
     res->which_pl = response_adc_chs_get_tag;
-    res->hdr.ret = ret_code_OK;
+    response_set_hdr(res, command_id_ADC_CHS_GET, ret_code_OK, NULL);
 
     return 0;
 }
@@ -64,18 +76,18 @@ int cmd_interpreter_decode(char *buffer, int buffer_len, request *req)
     return 0;
 }
 
-int cmd_interpreter_encode(char *buffer, int *buffer_len, response *res)
+int cmd_interpreter_encode(char *buffer, int buffer_len_max, int *buffer_len, response *res)
 {
     int status;
 
-    pb_ostream_t o_stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_ostream_t o_stream = pb_ostream_from_buffer(buffer, buffer_len_max);
     status = pb_encode(&o_stream, response_fields, res);
-    *buffer_len = o_stream.bytes_written;
     if (!status)
     {
         LOG_ERR("Encoding failed: %s\n", PB_GET_ERROR(&o_stream));
         return -1;
     }
+    *buffer_len = o_stream.bytes_written;
 
     return 0;
 }
@@ -88,4 +100,3 @@ cmd_hndlr_t *cmd_interpreter_get(void)
 
     return cmd_handlers;
 }
-
--- src/cmd_interpreter.h
+++ src/cmd_interpreter.h
@@ -8,3 +8,5 @@ typedef int (*cmd_hndlr_t) (const request *req, response *res);
 
 /***************************** INTERFACE FUNCTIONS ****************************/
 cmd_hndlr_t *cmd_interpreter_get(void);
+int cmd_interpreter_decode(char *buffer, int buffer_len, request *req);
+int cmd_interpreter_encode(char *buffer, int buffer_len_max, int *buffer_len, response *res);
