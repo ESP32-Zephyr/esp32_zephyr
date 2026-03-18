c3a8b63a278217bda28856f96b42a2141cb5f278
Iliya Iliev <iliyailiev3592@gmail.com>
2026-03-18 09:50:20 +0200

    cmake: cmake-git-version-tracking.cmake: Use latest master
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- cmake/cmake-git-version-tracking.cmake
+++ cmake/cmake-git-version-tracking.cmake
@@ -3,5 +3,5 @@ include(FetchContent)
 set(VERSION_TRACKING_MODULE_PATH "" CACHE STRING "The location of the cmake-git-version-tracking repository")
 FetchContent_Declare(cmake_git_version_tracking
     GIT_REPOSITORY https://github.com/andrew-hardin/cmake-git-version-tracking.git
-    GIT_TAG        7d9442bfade5a47b77291e4117f81cc65887c7b9)
+    GIT_TAG        master)
 FetchContent_MakeAvailable(cmake_git_version_tracking)
